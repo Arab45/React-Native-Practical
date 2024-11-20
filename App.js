32d2a5663162df0a2034eceb7a8cef2d882682c1
Arab45 <horlakunleolayiwola@gmail.com>
2024-11-20 15:43:50 +0000

    sign in page

--- App.js
+++ App.js
@@ -1,90 +1,148 @@
-import React, { useState } from 'react';
-import {  Text, View, Image, StyleSheet, Pressable } from 'react-native';
-import { demoData } from './assets/data/data';
+import React, { useState } from "react";
+import { Text, View, Image, StyleSheet, Pressable } from "react-native";
+import { TextInput } from "react-native-web";
 
+const arrow = require("./assets/icon.png");
 
 const cb = () => {
-  console.log('sign up!')
+  console.log("sign up!");
 };
-
 
 export default function App() {
-  let pageImage = require('./assets/pageImage.png')
+  let { email, setEmail } = useState("");
+  let { password, setPassword } = useState("");
   return (
     <View style={style.header}>
-      <View style={style.imageHeader}>
-      <Image source={pageImage} style={style.image}/>
+      <View style={style.headerCont}>
+        <Image source={arrow} style={{ height: 20, width: 20 }} />
+        <Text style={style.parentText}>Sign In</Text>
       </View>
-      <View style={style.parentContainer}>
-        <Text style={style.firstText}>You'll Find</Text>
-        <View style={style.unique}>
-        <Text style={style.secondText}>All you need</Text>
-        <View style={style.horizontal}></View>
+      <View>
+        <Text style={style.label}>E-mail</Text>
+        <TextInput
+          placeholder="Example@gmail.com"
+          value={email}
+          onChange={setEmail}
+          style={style.emailInput}
+        />
+      </View>
+      <View>
+        <Text style={style.label}>Password</Text>
+        <TextInput
+          placeholder="Password"
+          value={password}
+          onChange={setPassword}
+          style={style.emailInput}
+        />
+      </View>
+      <Pressable onPress={cb} style={style.button}>
+        <Text style={style.buttonText}>Sign In</Text>
+      </Pressable>
+      <View
+        style={{
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "center",
+          gap: 10,
+        }}
+      >
+        <View style={{ height: 2, width: 90, backgroundColor: "#ddd" }}></View>
+        <View>
+          <Text style={{ color: "#273991", fontWeight: "400" }}>
+            Or sign in with
+          </Text>
         </View>
-        <Text style={style.thirdText}>Here!</Text>
+        <View
+          style={{ height: 2, width: 90, backgroundColor: "#ddd", gap: 8 }}
+        ></View>
       </View>
-        <Pressable onPress={cb} style={style.button}>
-          <Text style={style.text}>Sign Up</Text>
-        </Pressable>
-      <Text style={{color: '#4F63AC', fontWeight: 'bold', fontSize: 16}}>Sign In</Text>
+      <View style={style.google}>
+        <Image
+          source={{
+            uri: "https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/google-white-icon.png",
+          }}
+          style={style.imageStyle}
+        />
+      </View>
+      <View
+        style={{
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Text style={{ color: "#273991" }}>Don't have an account?</Text>
+        <Text style={{ fontSize: 17, fontWeight: "400", color: "#273991" }}>
+          {" "}
+          Sign Up
+        </Text>
+      </View>
     </View>
   );
-};
+}
 
 //space-between
 
 const style = StyleSheet.create({
-header: {
-  flex: 1,
-  backgroundColor: '#fff',
-  alignItems: 'center',
-  gap: 32,
-  paddingHorizontal: 64
-  // justifyContent: 'center'
-},
-imageHeader: {
-  paddingTop: 100,
-},
-image: {
-  height: 209,
-  width: 357,
-},
-parentContainer: {
-  alignItems: 'center',
-  paddingHorizontal: 20
-},
-firstText: {
-  fontSize: 10,
-  fontWeight: 'bold',
-  fontSize: 25,
-  color: '#303030'
-},
-secondText:{
-color: "#FCA34D",
-fontSize: 30,
-fontWeight: 500
-},
-horizontal: {
-  height: 2,
-  width: 167,
-  backgroundColor: "#FCA34D"
-},
-thirdText: {
-  fontSize: 10,
-  fontWeight: 'bold',
-  color: '#303030',
-  fontSize: 25,
-}, 
-button: {
-  backgroundColor: '#4F63AC',
-  padding: 12,
-  width: 280,
-  borderRadius: 8
-},
-text: {
-  color: '#fff',
-  fontSize: 16,
-  fontWeight: 'bold',
-  textAlign: 'center'
-}
+  header: {
+    flex: 1,
+    backgroundColor: "#fff",
+    paddingHorizontal: 20,
+    // alignItems: "center",
+    maxWidth: 310,
+    margin: "auto",
+    // justifyContent: 'center',
+    marginTop: 64,
+    gap: 24,
+  },
+  headerCont: {
+    flexDirection: "row",
+    gap: 20,
+    alignItems: "center",
+    width: "100%",
+    // paddingLeft: 20,
+  },
+  parentText: {
+    fontSize: 26,
+    fontWeight: "500",
+    color: "#4F63AC",
+  },
+  emailInput: {
+    // backgroundColor: '#fff',
+    // width: 303,
+    // height: 86,
+    paddingVertical: 20,
+    paddingHorizontal: 10,
+    borderRadius: 16,
+    borderWidth: 1,
+    borderColor: "#8D9BB5",
+  },
+  label: {
+    color: "#4F63AC",
+    fontSize: 18,
+    marginBottom: 8
+  },
+  button: {
+    backgroundColor: "#4F63AC",
+    height: 60,
+    borderRadius: 15,
+    justifyContent: "center",
+  },
+  buttonText: {
+    color: "#fff",
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  google: {
+    backgroundColor: "#404040",
+    paddingVertical: 15,
+    width: 142,
+    alignItems: "center",
+    borderRadius: 15,
+    margin: 'auto'
+  },
+  imageStyle: {
+    height: 20,
+    width: 20,
+  },
 });
---
+++ splash-page.js
@@ -0,0 +1,90 @@
+import React, { useState } from 'react';
+import {  Text, View, Image, StyleSheet, Pressable } from 'react-native';
+import { demoData } from './assets/data/data';
+
+
+const cb = () => {
+  console.log('sign up!')
+};
+
+
+export default function App() {
+  let pageImage = require('./assets/pageImage.png')
+  return (
+    <View style={style.header}>
+      <View style={style.imageHeader}>
+      <Image source={pageImage} style={style.image}/>
+      </View>
+      <View style={style.parentContainer}>
+        <Text style={style.firstText}>You'll Find</Text>
+        <View style={style.unique}>
+        <Text style={style.secondText}>All you need</Text>
+        <View style={style.horizontal}></View>
+        </View>
+        <Text style={style.thirdText}>Here!</Text>
+      </View>
+        <Pressable onPress={cb} style={style.button}>
+          <Text style={style.text}>Sign Up</Text>
+        </Pressable>
+      <Text style={{color: '#4F63AC', fontWeight: 'bold', fontSize: 16}}>Sign In</Text>
+    </View>
+  );
+};
+
+//space-between
+
+const style = StyleSheet.create({
+header: {
+  flex: 1,
+  backgroundColor: '#fff',
+  alignItems: 'center',
+  gap: 32,
+  paddingHorizontal: 64
+  // justifyContent: 'center'
+},
+imageHeader: {
+  paddingTop: 100,
+},
+image: {
+  height: 209,
+  width: 357,
+},
+parentContainer: {
+  alignItems: 'center',
+  paddingHorizontal: 20
+},
+firstText: {
+  fontSize: 10,
+  fontWeight: 'bold',
+  fontSize: 25,
+  color: '#303030'
+},
+secondText:{
+color: "#FCA34D",
+fontSize: 30,
+fontWeight: 500
+},
+horizontal: {
+  height: 2,
+  width: 167,
+  backgroundColor: "#FCA34D"
+},
+thirdText: {
+  fontSize: 10,
+  fontWeight: 'bold',
+  color: '#303030',
+  fontSize: 25,
+}, 
+button: {
+  backgroundColor: '#4F63AC',
+  padding: 12,
+  width: 280,
+  borderRadius: 8
+},
+text: {
+  color: '#fff',
+  fontSize: 16,
+  fontWeight: 'bold',
+  textAlign: 'center'
+}
+});
